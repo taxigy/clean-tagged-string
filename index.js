879d88b79a495afe991d4fdbeb61e21dace74ffe
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2016-03-10 23:59:49 +0400

    Hi.

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = clean;
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function clean(strings) {
+  if (!strings.raw) {
+    throw new Error('Use clean as a tag for template string, e.g. clean`123`, not clean(`123`).');
+  } else {
+    for (var _len = arguments.length, values = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+      values[_key - 1] = arguments[_key];
+    }
+
+    var joined = _lodash2.default.join(_lodash2.default.flatten(_lodash2.default.zip(strings, values)), '');
+    var cleaned = _lodash2.default.replace(joined, /([.,!?])\s*([.,!?])/g, '$2');
+    var trimmed = _lodash2.default.replace(cleaned, /^\s*(.*)\s*$/, '$1');
+    var shortened = _lodash2.default.replace(trimmed, /\s{2,}/g, ' ');
+
+    return shortened;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+
+export default function clean(strings, ...values) {
+  if (!strings.raw) {
+    throw new Error('Use clean as a tag for template string, e.g. clean`123`, not clean(`123`).');
+  } else {
+    const joined = _.join(_.flatten(_.zip(strings, values)), '');
+    const cleaned = _.replace(joined, /([.,!?])\s*([.,!?])/g, '$2');
+    const trimmed = _.replace(cleaned, /^\s*(.*)\s*$/, '$1');
+    const shortened = _.replace(trimmed, /\s{2,}/g, ' ');
+
+    return shortened;
+  }
+}
