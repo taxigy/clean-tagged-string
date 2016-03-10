82fd17223643e8efdd86081192171c02dfcd9f5d
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2016-03-11 00:23:56 +0400

    The "Hi ${null}." -> "Hi." case.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function clean(strings) {
     }
 
     var joined = _lodash2.default.join(_lodash2.default.flatten(_lodash2.default.zip(strings, values)), '');
-    var cleaned = _lodash2.default.replace(joined, /([.,!?])\s*([.,!?])/g, '$2');
+    var cleaned = _lodash2.default.replace(joined, /([.,!?])?\s*([.,!?])/g, '$2');
     var trimmed = _lodash2.default.replace(cleaned, /^\s*(.*)\s*$/, '$1');
     var shortened = _lodash2.default.replace(trimmed, /\s{2,}/g, ' ');
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export default function clean(strings, ...values) {
     throw new Error('Use clean as a tag for template string, e.g. clean`123`, not clean(`123`).');
   } else {
     const joined = _.join(_.flatten(_.zip(strings, values)), '');
-    const cleaned = _.replace(joined, /([.,!?])\s*([.,!?])/g, '$2');
+    const cleaned = _.replace(joined, /([.,!?])?\s*([.,!?])/g, '$2');
     const trimmed = _.replace(cleaned, /^\s*(.*)\s*$/, '$1');
     const shortened = _.replace(trimmed, /\s{2,}/g, ' ');
 
